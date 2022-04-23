9be30512cfbb8013cceb5bc932bd8c0b29b98bdd
Johnexzy <obajohn75@gmail.com>
2022-04-23 13:52:55 +0100

    feat: token generates specific access

--- test/tokengate-test.js
+++ test/tokengate-test.js
@@ -17,51 +17,6 @@ describe("TokenGate", function () {
   before(async function () {
     await initialize();
   });
-  it("Should return false if user is not subscribed", async function () {
-    const TokenGateContract = await ContractObj.TokenGate.attach(
-      ContractObj.proxyAddress
-    );
-    const [owner] = await ethers.getSigners();
-    console.log(owner.address);
-    expect(await TokenGateContract.checkAccess(owner.address)).to.be.false;
-  });
-  it("Initialize Access Token", async function () {
-    const TokenGateContract = await ContractObj.TokenGate.attach(
-      ContractObj.proxyAddress
-    );
-    // ethers.Contract.
-    // const addr = (await ethers.getSigners())[1];
-    await expect(
-      TokenGateContract.initializeAccessToken([
-        "0x71C3022E585f138BABee7E3B8Eca15D058a17fEf", // contract address
-        ethers.utils.formatBytes32String("ERC721").toString(), // typeOfcontract bytes32
-        -1, // no specific id
-        true, // lifetime access
-        1, // amount (owns >= 1)
-      ])
-    )
-      .to.emit(TokenGateContract, "AddedAccessToken")
-      .withArgs(
-        "0x71C3022E585f138BABee7E3B8Eca15D058a17fEf",
-        ethers.utils.formatBytes32String("ERC721").toString(),
-        "-1",
-        "1"
-      );
-    // expect(TokenGateContract.setFee);
-  });
-
-  it("Deployer has admin role", async function () {
-    const TokenGateContract = await ContractObj.TokenGate.attach(
-      ContractObj.proxyAddress
-    );
-    const [owner] = await ethers.getSigners();
-    expect(
-      await TokenGateContract.hasRole(
-        ethers.utils.formatBytes32String(0x00),
-        owner.address
-      )
-    ).to.be.true;
-  });
   it("Deployer can grant sub-roles to accounts", async function () {
     const TokenGateContract = await ContractObj.TokenGate.attach(
       ContractObj.proxyAddress
@@ -86,32 +41,157 @@ describe("TokenGate", function () {
       )
     ).to.be.true;
   });
+  it("Deployer has admin role", async function () {
+    const TokenGateContract = await ContractObj.TokenGate.attach(
+      ContractObj.proxyAddress
+    );
+    const [owner] = await ethers.getSigners();
+    expect(
+      await TokenGateContract.hasRole(
+        ethers.utils.formatBytes32String(0x00),
+        owner.address
+      )
+    ).to.be.true;
+  });
 
-  /** Subscription **/
-  it("Set Subscription Fee", async function () {
+  it("Should return false, user is not subscribed or owns access token 0x71C3...7fEf", async function () {
+    const TokenGateContract = await ContractObj.TokenGate.attach(
+      ContractObj.proxyAddress
+    );
+    const [owner] = await ethers.getSigners();
+    expect(
+      await TokenGateContract.checkAccess(
+        "0x71C3022E585f138BABee7E3B8Eca15D058a17fEf",
+        owner.address
+      )
+    ).to.be.false;
+  });
+  it("Add ERC721 token as access.", async function () {
     const TokenGateContract = await ContractObj.TokenGate.attach(
       ContractObj.proxyAddress
     );
     // ethers.Contract.
     // const addr = (await ethers.getSigners())[1];
     await expect(
-      TokenGateContract.setFee(ethers.utils.parseEther("0.1"), "MONTHLY")
+      TokenGateContract.initializeAccessToken([
+        "0x71C3022E585f138BABee7E3B8Eca15D058a17fEf", // contract address
+        ethers.utils.formatBytes32String("ERC721").toString(), // typeOfcontract bytes32
+        -1, // no specific id
+        [ethers.utils.parseEther("0.1"), 2592000], // lifetime access
+        1, // amount (owns >= 1)
+      ])
+    )
+      .to.emit(TokenGateContract, "AddedAccessToken")
+      .withArgs(
+        "0x71C3022E585f138BABee7E3B8Eca15D058a17fEf",
+        ethers.utils.formatBytes32String("ERC721").toString(),
+        "-1",
+        "1",
+        ethers.utils.parseEther("0.1").toString(),
+        "2592000"
+      );
+    // expect(TokenGateContract.setFee);
+  });
+  it("Checking Access Should return true, user owns this access token: 0x71C3...7fEf", async function () {
+    const TokenGateContract = await ContractObj.TokenGate.attach(
+      ContractObj.proxyAddress
+    );
+
+    const [owner] = await ethers.getSigners();
+    // console.log(owner.address);
+    expect(
+      await TokenGateContract.checkAccess(
+        "0x71C3022E585f138BABee7E3B8Eca15D058a17fEf",
+        owner.address
+      )
+    ).to.be.true;
+    // expect(TokenGateContract.setFee);
+  });
+
+  it("Disable 0x71C3...7fEf from access tokens", async function () {
+    const TokenGateContract = await ContractObj.TokenGate.attach(
+      ContractObj.proxyAddress
+    );
+
+    // console.log(owner.address);
+    await expect(
+      TokenGateContract.disableTokenAccess(
+        "0x71C3022E585f138BABee7E3B8Eca15D058a17fEf"
+      )
+    )
+      .to.emit(TokenGateContract, "DisableTokenAccess")
+      .withArgs("0x71C3022E585f138BABee7E3B8Eca15D058a17fEf");
+    // expect(TokenGateContract.setFee);
+  });
+  it("Checking Access for 0x71C3...7fEf should return false, no access token initialized and user is not subscribed", async function () {
+    const TokenGateContract = await ContractObj.TokenGate.attach(
+      ContractObj.proxyAddress
+    );
+
+    const [owner] = await ethers.getSigners();
+    // console.log(owner.address);
+    expect(
+      await TokenGateContract.checkAccess(
+        "0x71C3022E585f138BABee7E3B8Eca15D058a17fEf",
+        owner.address
+      )
+    ).to.be.false;
+    // expect(TokenGateContract.setFee);
+  });
+  /** Subscription **/
+  it("Set Subscription Fee for  users without this 0x71C3...7fEf token", async function () {
+    const TokenGateContract = await ContractObj.TokenGate.attach(
+      ContractObj.proxyAddress
+    );
+    // ethers.Contract.
+    // const addr = (await ethers.getSigners())[1];
+    await expect(
+      TokenGateContract.setFee(
+        "0x71C3022E585f138BABee7E3B8Eca15D058a17fEf",
+        ethers.utils.parseEther("0.05"),
+        7 // 7days
+      )
     )
       .to.emit(TokenGateContract, "SetFee")
-      .withArgs(ethers.utils.parseEther("0.1").toString(), "MONTHLY");
+      .withArgs(
+        "0x71C3022E585f138BABee7E3B8Eca15D058a17fEf",
+        ethers.utils.parseEther("0.05").toString(),
+        "604800"
+      );
   });
-  it("User can Subscribe", async function () {
+  it("User can Subscribe by paying Fee", async function () {
     const TokenGateContract = await ContractObj.TokenGate.attach(
       ContractObj.proxyAddress
     );
 
     // const [owner] = await ethers.getSigners();
     // expect(await TokenGateContract.checkIfSubscribed(owner.address)).to.be.false;
-    const options = { value: ethers.utils.parseEther("0.1") };
-    await expect(TokenGateContract.subscribe(options)).to.emit(
-      TokenGateContract,
-      "Subscribed"
+    let fee = await TokenGateContract.getFeeForTokenAccess(
+      "0x71C3022E585f138BABee7E3B8Eca15D058a17fEf"
     );
+    fee = ethers.utils.formatEther(fee.price);
+    const options = { value: ethers.utils.parseEther(fee) };
+    await expect(
+      TokenGateContract.subscribe(
+        "0x71C3022E585f138BABee7E3B8Eca15D058a17fEf",
+        options
+      )
+    ).to.emit(TokenGateContract, "Subscribed");
+  });
+  it("Checking Access for 0x71C3...17fEf should return true, user paid for subscription,", async function () {
+    const TokenGateContract = await ContractObj.TokenGate.attach(
+      ContractObj.proxyAddress
+    );
+
+    const [owner] = await ethers.getSigners();
+    // console.log(owner.address);
+    expect(
+      await TokenGateContract.checkAccess(
+        "0x71C3022E585f138BABee7E3B8Eca15D058a17fEf",
+        owner.address
+      )
+    ).to.be.true;
+    // expect(TokenGateContract.setFee);
   });
   it("Withdraw ETH from contract", async function () {
     const TokenGateContract = await ContractObj.TokenGate.attach(
